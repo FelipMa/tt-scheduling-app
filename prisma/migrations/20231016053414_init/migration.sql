8a86f339f4339339a978633793b40fe45b5ba8ed
FelipMa <felipeapenburg@gmail.com>
2023-10-16 02:42:04 -0300

    feat postgres

--- prisma/migrations/20231016053414_init/migration.sql
+++ prisma/migrations/20231016053414_init/migration.sql
@@ -1,10 +1,12 @@
 -- CreateTable
 CREATE TABLE "Schedule" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "targetDate" TEXT NOT NULL,
     "text" TEXT NOT NULL,
     "reply" TEXT NOT NULL,
     "media" TEXT NOT NULL,
     "accountUsername" TEXT NOT NULL,
-    "status" TEXT NOT NULL
+    "status" TEXT NOT NULL,
+
+    CONSTRAINT "Schedule_pkey" PRIMARY KEY ("id")
 );
